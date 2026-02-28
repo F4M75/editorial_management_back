c6f2391153f569202478ff8ef1462f4df1d4256f
Famenontsoa RALAHADY <famenontsoaralahady@gmail.com>
2026-02-28 20:26:00 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import router from './routes';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+export default app;
